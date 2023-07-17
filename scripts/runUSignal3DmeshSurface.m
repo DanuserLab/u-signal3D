bcf82be7a94fdbb1ab88bc9bb3a9121c32387b08
Danuser CI User <danuser_ci@NucleusC029.cm.cluster>
2023-07-17 14:47:24 -0500

    Updated scripts to the latest version, added two missing fcns from buildpackage.

--- scripts/runUSignal3DmeshSurface.m
+++ scripts/runUSignal3DmeshSurface.m
@@ -1,16 +1,14 @@
 function runUSignal3DmeshSurface()
 
 %imageDirectory is for raw data
-% imageDirectory='/project/bioinformatics/Danuser_lab/3Dmorphogenesis/analysis/Hanieh/SpectralDecomposition/Test_Package_Jenny/raw/hiRes3D/ASLM/MV3/AktPH_Collagen/PI3K_inhibit/Control/200127'
 imageDirectory='/project/bioinformatics/Danuser_lab/3Dmorphogenesis/analysis/Hanieh/SpectralDecomposition/Examples/Example2/testData';
-saveDirectory='/project/bioinformatics/Danuser_lab/3Dmorphogenesis/analysis/Hanieh/SpectralDecomposition/Examples/Example2/analysisOrganizedJuly14PLy';
-psfDirectory = '/project/bioinformatics/Danuser_lab/3Dmorphogenesis/analysis/Hanieh/SpectralDecomposition/Examples/Example2/PSFreduced/'; % directory of microscope PSFs
+saveDirectory='/project/bioinformatics/Danuser_lab/3Dmorphogenesis/analysis/Hanieh/SpectralDecomposition/Examples/Example2/analysisPly';
 
-imageList=[1];
-pathPSF = [psfDirectory filesep 'rotAvgPSF.mat'];
-%mesh name
+imageList=[1 2];
+%mesh name, mesh saved in another package as ply or obj and here is the
+%input
 meshFilename = 'surfacemesh.ply';
-% meshFilename = 'surfacemesh.obj';
+% meshFilename = 'surfacemesh.obj'; %
 % set the parameters
 pixelSizeXY=160.0990; %PI3K data
 pixelSizeZ=160;
@@ -28,7 +26,7 @@ for iCell=1:length(imageList)
     %case 1 - when I have two channels
     BFDataPath = [imageDirectory '/Cell' num2str(imageList(iCell)) filesep imageName];
     ResultPath = [saveDirectory '/Cell' num2str(imageList(iCell))];
-    if ~isdir(ResultPath) mkdir(ResultPath); end 
+    if ~isdir(ResultPath) mkdir(ResultPath); end
     MD = MovieData(BFDataPath, ResultPath);
     % case2 - for oneChannel folder
     % MD = makeMovieDataOneChannel(imagePathCell, savePathCell, pixelSizeXY, pixelSizeZ, timeInterval);
@@ -36,112 +34,114 @@ for iCell=1:length(imageList)
     % add a package
     MD.addPackage(uSignal3DPackage(MD)); % only for Initail script
     
-%% Process 1: Deconvolution3DProcess
-% % disp('===================================================================');
-% % disp('Running (1st) Deconvolution');
-% % disp('===================================================================');
-% % iPack = 1;
-% % step_ = 1;
-% % MD.getPackage(iPack).createDefaultProcess(step_)
-% % params = MD.getPackage(iPack).getProcess(step_).funParams_;
-% % params.deconMode = 'richLucy'; % Edit process parameter, tightness, from 0.5 to 0.6
-% % params.richLucyIter = 8;
-% % params.apoHeight=0;
-% % params.pathApoPSF = pathPSF;
-% % params.pathDeconPSF = pathPSF;
-% % params.ChannelIndex = 1; %analyze only channel1
-% % MD.getPackage(iPack).getProcess(step_).setPara(params);
-% % MD.save;
-% % params = MD.getPackage(iPack).getProcess(step_).funParams_
-% % MD.getPackage(iPack).getProcess(step_).run();
-% % 
-% % 
-% % %% Process 2: ComputeMIPProcess
-% % disp('===================================================================');
-% % disp('Running (2nd) Maximum Intensity Projection (MIP)');
-% % disp('===================================================================');
-% % iPack = 1;
-% % step_ = 2;
-% % MD.getPackage(iPack).createDefaultProcess(step_)
-% % params = MD.getPackage(iPack).getProcess(step_).funParams_;
-% % params.ChannelIndex = 1; %analyze only channel1
-% % MD.getPackage(iPack).getProcess(step_).setPara(params);
-% % MD.save;
-% % params = MD.getPackage(iPack).getProcess(step_).funParams_
-% % MD.getPackage(iPack).getProcess(step_).run();
-% % 
-% % 
-%% Process 3: Mesh3DProcess
-disp('===================================================================');
-disp('Running (3rd) Creating Mesh Surface');
-disp('===================================================================');
-iPack = 1;
-step_ = 3;
-MD.getPackage(iPack).createDefaultProcess(step_)
-params = MD.getPackage(iPack).getProcess(step_).funParams_;
-params.smoothMeshMode = 'curvature';
-params.scaleOtsu = 1;
-params.imageGamma = 0.7;
-params.smoothImageSize = 1.5;
-params.insideErodeRadius = 7;
-params.meshMode ='readPlyFile';
-params.plyFilePath =[imageDirectory filesep 'Cell' num2str(imageList(iCell)) filesep meshFilename];
-params.useUndeconvolved = 1; 
-params.removeSmallComponents = 1;
-params.ChannelIndex = 1; %analyze only channel1
-MD.getPackage(iPack).getProcess(step_).setPara(params);
-MD.save;
-params = MD.getPackage(iPack).getProcess(step_).funParams_
-MD.getPackage(iPack).getProcess(step_).run();
-
-%% Process 4: Intensity3DProcess
-disp('===================================================================');
-disp('Running (4th) Measuring Intensity on Vertices');
-disp('===================================================================');
-iPack = 1;
-step_ = 4;
-MD.getPackage(iPack).createDefaultProcess(step_)
-params = MD.getPackage(iPack).getProcess(step_).funParams_;
-params.sampleRadius = [1 1];
-params.rmInsideBackground = [0 ];
-params.meanNormalization = [1 ]; %it doesn't exclude the second channel for line 273
-params.intensityMode = {'intensityInsideRawVertex','intensityInsideRawVertex'};
-params.ChannelIndex = 1; %analyze only channel1
-MD.getPackage(iPack).getProcess(step_).setPara(params);
-MD.save;
-params = MD.getPackage(iPack).getProcess(step_).funParams_
-MD.getPackage(iPack).getProcess(step_).run();
-
-
-%% Process 5: LaplaceBeltrami3DProcess
-disp('===================================================================');
-disp('Running (5th) Computing Laplace-Beltrami Operator on Vertices');
-disp('===================================================================');
-iPack = 1;
-step_ = 5;
-
-MD.getPackage(iPack).createDefaultProcess(step_)
-params = MD.getPackage(iPack).getProcess(step_).funParams_;
-params.ChannelIndex = 1; %analyze only channel1
- params.nEigenvec = 100; 
-MD.getPackage(iPack).getProcess(step_).setPara(params);
-MD.save;
-params = MD.getPackage(iPack).getProcess(step_).funParams_
-MD.getPackage(iPack).getProcess(step_).run();
-
-
-%% Step 6: EnergySpectra3DProcess
-disp('===================================================================');
-disp('Running (6th) Calculating energy spectra');
-disp('===================================================================');
-iPack = 1;
-step_ = 6;
-
-MD.getPackage(iPack).createDefaultProcess(step_)
-params = MD.getPackage(iPack).getProcess(step_).funParams_;
-params.ChannelIndex = 1; %analyze only channel1
-MD.getPackage(iPack).getProcess(step_).setPara(params);
-MD.save;
-params = MD.getPackage(iPack).getProcess(step_).funParams_
-MD.getPackage(iPack).getProcess(step_).run();
-end 
+    %skip the first two processes because mesh has been created before
+    %% Process 1: Deconvolution3DProcess
+    % % disp('===================================================================');
+    % % disp('Running (1st) Deconvolution');
+    % % disp('===================================================================');
+    % % iPack = 1;
+    % % step_ = 1;
+    % % MD.getPackage(iPack).createDefaultProcess(step_)
+    % % params = MD.getPackage(iPack).getProcess(step_).funParams_;
+    % % params.deconMode = 'richLucy'; % Edit process parameter, tightness, from 0.5 to 0.6
+    % % params.richLucyIter = 8;
+    % % params.apoHeight=0;
+    % % params.pathApoPSF = pathPSF;
+    % % params.pathDeconPSF = pathPSF;
+    % % params.ChannelIndex = 1; %analyze only channel1
+    % % MD.getPackage(iPack).getProcess(step_).setPara(params);
+    % % MD.save;
+    % % params = MD.getPackage(iPack).getProcess(step_).funParams_
+    % % MD.getPackage(iPack).getProcess(step_).run();
+    % %
+    % %
+    % % %% Process 2: ComputeMIPProcess
+    % % disp('===================================================================');
+    % % disp('Running (2nd) Maximum Intensity Projection (MIP)');
+    % % disp('===================================================================');
+    % % iPack = 1;
+    % % step_ = 2;
+    % % MD.getPackage(iPack).createDefaultProcess(step_)
+    % % params = MD.getPackage(iPack).getProcess(step_).funParams_;
+    % % params.ChannelIndex = 1; %analyze only channel1
+    % % MD.getPackage(iPack).getProcess(step_).setPara(params);
+    % % MD.save;
+    % % params = MD.getPackage(iPack).getProcess(step_).funParams_
+    % % MD.getPackage(iPack).getProcess(step_).run();
+    % %
+    % %
+    
+    %% Process 3: Mesh3DProcess
+    % we still need to run the mesh process to save the mesh as in input for
+    % the next processes
+    disp('===================================================================');
+    disp('Running (3rd) Creating Mesh Surface');
+    disp('===================================================================');
+    iPack = 1;
+    step_ = 3;
+    MD.getPackage(iPack).createDefaultProcess(step_)
+    params = MD.getPackage(iPack).getProcess(step_).funParams_;
+    % params.smoothMeshMode = 'curvature';
+    % params.scaleOtsu = 1;
+    % params.imageGamma = 0.7;
+    % params.smoothImageSize = 1.5;
+    % params.insideErodeRadius = 7;
+    params.meshMode ='readPlyFile';
+    params.plyFilePath =[imageDirectory filesep 'Cell' num2str(imageList(iCell)) filesep meshFilename];
+    params.useUndeconvolved = 1; % for not searching
+    params.removeSmallComponents = 1;
+    params.ChannelIndex = 1; %analyze only channel1
+    MD.getPackage(iPack).getProcess(step_).setPara(params);
+    MD.save;
+    params = MD.getPackage(iPack).getProcess(step_).funParams_
+    MD.getPackage(iPack).getProcess(step_).run();
+    
+    %% Process 4: Intensity3DProcess
+    disp('===================================================================');
+    disp('Running (4th) Measuring Intensity on Vertices');
+    disp('===================================================================');
+    iPack = 1;
+    step_ = 4;
+    MD.getPackage(iPack).createDefaultProcess(step_)
+    params = MD.getPackage(iPack).getProcess(step_).funParams_;
+    params.sampleRadius = [1 1];
+    params.rmInsideBackground = [0 ];
+    params.meanNormalization = [1 ]; %it doesn't exclude the second channel for line 273
+    params.intensityMode = {'intensityInsideRawVertex','intensityInsideRawVertex'};
+    params.ChannelIndex = 1; %analyze only channel1
+    MD.getPackage(iPack).getProcess(step_).setPara(params); 
+    MD.save;
+    params = MD.getPackage(iPack).getProcess(step_).funParams_
+    MD.getPackage(iPack).getProcess(step_).run();
+    
+    
+    %% Process 5: LaplaceBeltrami3DProcess
+    disp('===================================================================');
+    disp('Running (5th) Computing Laplace-Beltrami Operator on Vertices');
+    disp('===================================================================');
+    iPack = 1;
+    step_ = 5;
+    MD.getPackage(iPack).createDefaultProcess(step_)
+    params = MD.getPackage(iPack).getProcess(step_).funParams_;
+    params.ChannelIndex = 1; %analyze only channel1
+    params.nEigenvec = 100;
+    MD.getPackage(iPack).getProcess(step_).setPara(params);
+    MD.save;
+    params = MD.getPackage(iPack).getProcess(step_).funParams_
+    MD.getPackage(iPack).getProcess(step_).run();
+    
+    
+    %% Step 6: EnergySpectra3DProcess
+    disp('===================================================================');
+    disp('Running (6th) Calculating energy spectra');
+    disp('===================================================================');
+    iPack = 1;
+    step_ = 6;
+    MD.getPackage(iPack).createDefaultProcess(step_)
+    params = MD.getPackage(iPack).getProcess(step_).funParams_;
+    params.ChannelIndex = 1; %analyze only channel1
+    MD.getPackage(iPack).getProcess(step_).setPara(params);
+    MD.save;
+    params = MD.getPackage(iPack).getProcess(step_).funParams_
+    MD.getPackage(iPack).getProcess(step_).run();
+end
